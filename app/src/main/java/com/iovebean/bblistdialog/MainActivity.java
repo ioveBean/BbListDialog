eee282f790863f1c0e2c38a74ca3d322840b2b20
binbin <binbin@binbin.com>
2019-12-06 22:53:09 +0800

    Initial commit

---
+++ app/src/main/java/com/iovebean/bblistdialog/MainActivity.java
@@ -0,0 +1,98 @@
+package com.iovebean.bblistdialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedHashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void textOnclicK(View view) {
+        ArrayList<String> strings = new ArrayList<>();
+        strings.add("第一张");
+        strings.add("第二张");
+        new MyUtils().showAlertList(this, strings,"目录",R.layout.buttonslist,R.id.buttongrid,
+                R.layout.gridview_item,R.id.buttons,0,
+                new MyUtils.ResultListner() {
+            @Override
+            public void getResult(String text) {
+
+                if(text.equals("第一张")){
+                    Log.e("tag","第一张");
+                    doText2();
+                }
+            }
+                 /*   window.setGravity(Gravity.CENTER); 中间位置
+                    window.setGravity(Gravity.BOTTEM); 底部位置
+                    window.setGravity(Gravity.TOP); 顶部位置 */
+
+                    @Override
+                    public void getDialog(AlertDialog btdialog) {
+                        DisplayMetrics dm = new DisplayMetrics();
+                        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+                        btdialog.getWindow().setGravity(Gravity.LEFT);
+                        btdialog.getWindow().setLayout(dm.widthPixels/3, dm.heightPixels);
+
+                    }
+
+                    @Override
+                    public int setStyleId() {
+                        return R.style.DialogWhenLarge;
+                    }
+                });
+    }
+
+    private void doText2() {
+        LinkedHashMap linkedHashMap = new LinkedHashMap();
+        linkedHashMap.put(R.drawable.emotion_icon_23_popup,"1");
+        linkedHashMap.put(R.drawable.emotion_icon_24_popup,"2");
+        new MyUtils().showAlertList(this, linkedHashMap,"    ",R.layout.buttonslist2,R.id.buttongrid,
+                R.layout.gridview_item2,R.id.text,R.id.img,
+                new MyUtils.ResultListner() {
+            @Override
+            public void getResult(String text) {
+
+                if(text.equals("1")){
+                    Log.e("tag","1");
+
+                }
+            }
+
+                @Override public void getDialog(AlertDialog btdialog) {
+
+                    //这个可设置大小
+
+                    DisplayMetrics dm = new DisplayMetrics();
+                    getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+                    btdialog.getWindow().setGravity(Gravity.LEFT);
+                    btdialog.getWindow().setLayout(dm.widthPixels/2, dm.heightPixels);
+
+
+                }
+
+                    @Override
+                    public int setStyleId() {
+                        return R.style.DialogWhenLarge;
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/iovebean/bblistdialog/MyUtils.java
@@ -0,0 +1,162 @@
+package com.iovebean.bblistdialog;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.os.AsyncTask;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.SimpleAdapter;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+public class MyUtils {
+
+    public interface ResultListner{
+        void getResult(String text);
+        void getDialog(AlertDialog btdialog);
+        int setStyleId();
+    }
+    private  android.app.AlertDialog btdialog;
+    private  ArrayList<Map<String, Object>> dataList;
+
+    public <T> void showAlertList(Activity activity,T list, String title,
+                                  int layoutid,int gridid,
+                                  int layoutitem,int textid,int imageid,
+                                  final ResultListner resultListner){
+
+        showclicklist(activity,resultListner,list,title,layoutid,gridid,layoutitem,textid,imageid, new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                String text = dataList.get(arg2).get("text").toString();
+                resultListner.getResult(text);
+                btdialog.dismiss();
+            }
+        });
+    }
+    private <T> void showclicklist(Activity activity,ResultListner resultListner,T name,String title,int layoutid,int gridId ,int layoutitem,int textid,int imageid,AdapterView.OnItemClickListener listener ) {
+
+            LayoutInflater inflater = LayoutInflater.from(activity);
+
+            View btDialogView = inflater.inflate(layoutid, null);
+            ViewGroup parentViewGroup = (ViewGroup) btDialogView.getParent();
+            if (parentViewGroup != null) {
+                parentViewGroup.removeAllViews();
+            }
+
+
+            GridView gridView = btDialogView.findViewById(gridId);
+            //图标下的文字
+        String[] from;
+
+        int[] to;
+
+                dataList = new ArrayList<>();
+                if(name instanceof ArrayList){
+                    ArrayList list = (ArrayList) name;
+                    for(int i=0;i<list.size();i++){
+                        Map<String, Object> map= new HashMap<>();
+                        map.put("img", "");
+                        map.put("text",list.get(i));
+                        dataList.add(map);
+                    }
+                   from= new String[]{"text"};
+                    to= new int[]{textid};
+
+                }else if(name instanceof HashSet){
+                    HashSet list = (HashSet) name;
+                    Iterator iterator = list.iterator();    
+                    while (iterator.hasNext()){
+                        Object next = iterator.next();
+                        Map<String, Object> map= new HashMap<>();
+                        map.put("img", "");
+                        map.put("text",next);
+                        dataList.add(map);
+                    }
+                    from= new String[]{"text"};
+                    to= new int[]{textid};
+                }else if(name instanceof LinkedHashMap){
+                    LinkedHashMap map1 = (LinkedHashMap) name;
+                    Iterator it = map1.entrySet().iterator();
+                    while (it.hasNext()) {
+                        Map.Entry entry = (Map.Entry) it.next();
+
+                        Map<String, Object> map= new HashMap<>();
+                        map.put("img", entry.getKey());
+                        map.put("text",entry.getValue());
+                        dataList.add(map);
+                    }
+                    from= new String[]{"img", "text"};
+
+                    to= new int[]{imageid, textid};
+                }else if(name instanceof  HashMap){
+                    HashMap map1 = (HashMap) name;
+                    Iterator it = map1.entrySet().iterator();
+                    while (it.hasNext()) {
+                        Map.Entry entry = (Map.Entry) it.next();
+
+                        Map<String, Object> map= new HashMap<>();
+                        map.put("img", entry.getKey());
+                        map.put("text",entry.getValue());
+                        dataList.add(map);
+                    }
+                    from= new String[]{"img", "text"};
+                    to= new int[]{imageid, textid};
+                }else if(name instanceof  String[]){
+                    String[] array = (String[]) name;
+
+                    for(int i=0;i<array.length;i++){
+                        Map<String, Object> map= new HashMap<>();
+                        map.put("img", "");
+                        map.put("text",array[i]);
+                        dataList.add(map);
+                    }
+                    from= new String[]{"text"};
+                    to= new int[]{textid};
+                }else {
+
+                    Log.e("myUtils","error");
+                    return;
+                }
+
+
+
+
+
+            SimpleAdapter adapter = new SimpleAdapter(activity, dataList,layoutitem, from, to);
+
+            gridView.setAdapter(adapter);
+
+            gridView.setOnItemClickListener(listener);
+           int id =  resultListner.setStyleId();
+
+           if(id==0){
+               btdialog = new android.app.AlertDialog.Builder(activity).setTitle(title).setView(btDialogView).create();
+
+           }else {
+               btdialog = new android.app.AlertDialog.Builder(activity,id).setTitle(title).setView(btDialogView).create();
+
+           }
+        btdialog.setCanceledOnTouchOutside(true);
+            btdialog.show();
+            resultListner.getDialog(btdialog);
+
+    }
+
+    }
